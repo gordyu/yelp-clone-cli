04257c49eebdaca31427cc4e79cbd5d60f04ba09
kamekazz <taveras78@gmail.com>
2019-01-25 07:26:58 -0800

    HOT & NEW BUSINESSES

---
+++ src/components/MainCard.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+
+ class MainCard extends Component {
+
+  render() {
+    const {title,categories,tag,reviews}= this.props;
+
+
+    return (
+      <div className="MainCard">
+        <div className="img"></div>
+        <div className="content">
+            <h4>{title}</h4>
+            <div className="review"><p>{reviews} reviews</p></div>
+            <p >{categories}</p>
+            <p>{tag}</p>
+            <div className="oprend"><i class="fas fa-fire"></i> Opened 6 weeks ago </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainCard)
+
+
--- src/components/pages/home/item/YelpTon.jsx
+++ src/components/pages/home/item/YelpTon.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import  CategoriaTomLin  from './CategoriaTomLin';
+import MainCard from '../../../MainCard';
 
+const data=[
+    {location:"Groton",
+        hotNnew:[
+            {title:'Egg-O-Holic',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+            {title:'TSAôCAA',reviews:5,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+            {title:'Egg-O-Holic',reviews:5,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''}
+        ]
+    },
+    {location:"San Francisco",
+        hotNnew:[
+            {title:'TSAôCAA',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+            {title:'Egg-O-Holic',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+            {title:'Egg-O-Holic',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+        ]
+    },
+    {location:"San Jose"},
+    {location:"Los Angeles"},
+    {location:"Chicago"},
+    {location:"Palo Alto"},
+    {location:"More Cities"},
+]
 
 
 
  class YelpTon extends Component {
+
+    state={
+        locationStores:0,
+        area:'Groton'
+    }
+
+    chanchArea =(prArea,prIndex)=>{
+        this.setState({area:prArea,locationStores:prIndex});
+    }
 
 
 
@@ -13,27 +44,52 @@ import  CategoriaTomLin  from './CategoriaTomLin';
 
 
   render() {
+    const {area,locationStores} = this.state;
+
+    const rdLocationLinst =()=>(
+        data.map( 
+            (lc, index) => {
+                let areaBoolen =false 
+            
+                if (area === lc.location ) {
+                    areaBoolen =true;
+                } else {
+                    areaBoolen =false;
+                }
+                
+                return(
+                    <li key={lc.location} onClick={()=>this.chanchArea(lc.location,index)} className={areaBoolen ? 'list__order--1 on':'list__order--1'}>{lc.location}</li>
+                )    
+            }
+        )
+    )
+
+    const rdHotnNewList =()=>(
+        data[0].hotNnew.map(
+            st => (
+                <MainCard key={st.title} title={st.title} reviews={st.reviews} categories={st.categories} tag={st.tag} img={st.img} />
+            )  
+        )
+    )
 
 
 
     return (
         <div className="yelpTon">
             <div className="main ">
-                <h2 className="heading-secondary">Yelp Groton</h2>
+                <h2 className="heading-secondary">Yelp {area}</h2>
                 <div className="list">
                     <ul className="list__order">
-                        <li className="list__order--1">San Francisco</li>
-                        <li className="list__order--2">New York</li>
-                        <li className="list__order--3">San Jose</li>
-                        <li className="list__order--4">Los Angeles</li>
-                        <li className="list__order--5">Chicago</li>
-                        <li className="list__order--6">Palo Alto</li>
-                        <li className="list__order--6">More Cities</li>
+                        {rdLocationLinst()}
                     </ul>
                 </div>
-                <div className="rowOfCategory">
-                   
+                <div className="hotNnewTitle">
+                    <h3 className="heading-tertiary ">Hot & New Businesses</h3>
+                    <div className="hotNnew">
+                        {rdHotnNewList()}
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
