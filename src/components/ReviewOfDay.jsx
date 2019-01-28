a3a30f2201861e9be7e858e233cdd56f12a728d3
kamekazz <taveras78@gmail.com>
2019-01-28 13:27:28 -0800

    fix  new businessses

--- src/components/ReviewOfDay.jsx
+++ src/components/ReviewOfDay.jsx
@@ -12,7 +12,10 @@ export default function ReviewOfDay({ data }) {
                 <p className="store">Wrote a review for <span  className="hilite">{data.store}</span></p>
             </div>
         </div>
-        <div className="blueLien">Review of the Day</div>
+        <div className="blueLien">Review of the Day
+        <img className="iconC" src="https://s3-media1.fl.yelpcdn.com/assets/srv0/yelp_styleguide/8bb21835695e/assets/img/badge_banner_icons/40x40_rotd.png" alt="https://s3-media1.fl.yelpcdn.com/assets/srv0/yelp_styleguide/8bb21835695e/assets/img/badge_banner_icons/40x40_rotd.png"/>
+        </div>
+        
         <div className="review"><RewiewStar review={data.reviews} /></div>
         <p className="paragraphR">{data.p}</p>
     </div>
