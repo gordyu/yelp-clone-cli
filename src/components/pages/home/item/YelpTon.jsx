452ec0f6db793821071aa5539b3aa5f7c39129e7
kamekazz <taveras78@gmail.com>
2019-01-28 13:33:20 -0800

    data

--- src/components/pages/home/item/YelpTon.jsx
+++ src/components/pages/home/item/YelpTon.jsx
@@ -21,22 +21,76 @@ const data=[
     },
     {location:"San Francisco",
         hotNnew:[
-            {title:'sdfgsdfg',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
-            {title:'Egg-O-sdfgdfs',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
-            {title:'Egg-O-sdfgsdf',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:''},
+            {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+            {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+            {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
         ],
-        reviewOfDay:{userName:'Manny W.',store:'Matcha Cafe Maiko',reviews:5,p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+        reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
         collections:[
-            {title:'sdfgsdfg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
-            {title:'hsdfgsdfgdg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
-            {title:'sfdgfds',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+            {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+            {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+            {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
         ]
     },
-    {location:"San Jose"},
-    {location:"Los Angeles"},
-    {location:"Chicago"},
-    {location:"Palo Alto"},
-    {location:"More Cities"},
+    {location:"San Jose",
+    hotNnew:[
+        {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+        {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+        {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
+    ],
+    reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+    collections:[
+        {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+    ]
+    },
+    {location:"Los Angeles",
+    hotNnew:[
+        {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+        {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+        {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
+    ],
+    reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+    collections:[
+        {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+    ]
+    },
+    {location:"Chicago",        hotNnew:[
+        {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+        {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+        {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
+    ],
+    reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+    collections:[
+        {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+    ]},
+    {location:"Palo Alto",        hotNnew:[
+        {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+        {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+        {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
+    ],
+    reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+    collections:[
+        {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+    ]},
+    {location:"More Cities",        hotNnew:[
+        {title:'Breakfast Little',reviews:4,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media2.fl.yelpcdn.com/bphoto/E4585gvylULHDE87fwXscA/l.jpg'},
+        {title:'TSAôCAA',reviews:2,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media1.fl.yelpcdn.com/bphoto/O22_26BtW-2-Ot0yXh3M_g/l.jpg'},
+        {title:'Egg-O-Holic',reviews:1,categories:['indian','vegetarian','halal'],tag:['River West', 'Noble Square','West Town'],img:'https://s3-media4.fl.yelpcdn.com/bphoto/zXe5ty27YbB0QN_ZBuqMXg/l.jpg'}
+    ],
+    reviewOfDay:{userName:'Dennis W.',store:'Matcha Cafe Maiko',reviews:5,img:'https://s3-media1.fl.yelpcdn.com/photo/8jH105M34nXMajrIWOOT-w/60s.jpg',p:"Located on the first floor in Japan Town's Japan Center in SF, the likely lengthy line will exactly pinpoint the store entrance for you. The shop itself is rather small and there are a few seats if you must stop to focus all of your energy on immediately consuming your sweet treat. For my first visit here, I went simple and ordered the Matcha soft serve in a waffle cone. I'm not what you would consider a big fan of M..."},
+    collections:[
+        {img:'https://s3-media4.fl.yelpcdn.com/bphoto/TwiEXQx26-499bxhNEGyTg/120s.jpg',title:'my',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/Dwq__xjGZF8rLqz7F8n-Gg/120s.jpg',title:'hydg',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'},
+        {img:'https://s3-media2.fl.yelpcdn.com/bphoto/2j8vWY35P0voUdJzrXtXRQ/120s.jpg',title:'fsss',p:"on the first floor in Japan Town's Japan Center in SF, the",places:23,by:'manny loro'}
+    ]},
 ]
 
 
