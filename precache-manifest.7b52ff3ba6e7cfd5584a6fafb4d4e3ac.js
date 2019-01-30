bcd08078d5a033b0f0b1a2c7eb12dbfa02d9e69f
kamekazz <taveras78@gmail.com>
2019-01-30 06:23:44 -0800

    Updates

---
+++ precache-manifest.7b52ff3ba6e7cfd5584a6fafb4d4e3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3193235f8f62f20a4a6d",
+    "url": "/static/css/main.3730a88b.chunk.css"
+  },
+  {
+    "revision": "3193235f8f62f20a4a6d",
+    "url": "/static/js/main.3193235f.chunk.js"
+  },
+  {
+    "revision": "c393ded5986431616c4b",
+    "url": "/static/js/1.c393ded5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c26c7d150eceb678c7c7afe20b4ec75b",
+    "url": "/index.html"
+  }
+];
